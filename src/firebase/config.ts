b61197d9bcab69b4063e062ec220a1054af031a7
Inky Digital <inkydigitaloficial@gmail.com>
2025-10-01 16:51:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1970230290-ec8e9",
+  "appId": "1:276974166869:web:375f18ee83c668eb0c45dc",
+  "apiKey": "AIzaSyApHswsbsLmZv2mfg5n-VW5qpt5TkbY5Gk",
+  "authDomain": "studio-1970230290-ec8e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "276974166869"
+};
