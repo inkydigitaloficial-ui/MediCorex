2b635eb59ad67387e65884ef834239f1ec437635
Inky Digital <inkydigitaloficial@gmail.com>
2025-10-02 17:50:13 +0000

    ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed: step exited with

---
+++ src/firebase/hooks.ts
@@ -0,0 +1,8 @@
+// Este é um arquivo "barrel" para exportar todos os hooks do Firebase de um só lugar.
+
+export { useUser } from '../lib/firebase/use-user';
+export { useDoc } from '../lib/firebase/use-doc';
+export { useCollection } from '../lib/firebase/use-collection';
+export { useAuth } from '../lib/firebase/use-auth';
+export { useFirestore } from '../lib/firebase/use-firestore';
+export { useMemoFirebase } from '../lib/firebase/use-memo-firebase';
